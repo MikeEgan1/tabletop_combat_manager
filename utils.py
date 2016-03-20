5013f2d0c995ceaab74bd51b0663bd3869a960d6
Mike <phegan@gmail.com>
2016-03-20 16:14:13 -0400

    removed the grave from trying to open my terminal

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 import random
 
 def create_hash():
-    return ''.j`oin(random.choice('0123456789ABCDEF') for i in xrange(16))
+    return ''.join(random.choice('0123456789ABCDEF') for i in xrange(16))
