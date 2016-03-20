95483dd1b1ac946c07657038c9ff367f0cfdc42c
Mike <phegan@gmail.com>
2016-03-20 15:52:56 -0400

    Added game functions and passing the the id back to the client

--- api/v1/game.py
+++ api/v1/game.py
@@ -1,7 +1,13 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
+from tabletop_combat_manager.game import create_new_game
 
 game = Blueprint('game', __name__)
 
 @game.route('/game/hello')
 def index():
     return "Hello, game!"
+
+@game.route('/game/new_game')
+def new_game():
+    return jsonify({'id' : create_new_game()})
+
