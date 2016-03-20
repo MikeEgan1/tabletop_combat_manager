89f594de3ae85ff3bf744708256fa2cc48ac6e63
Mike <phegan@gmail.com>
2016-03-20 15:32:08 -0400

    Added support for 404s

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
-#!usr/bin/python
-
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, world!"
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
