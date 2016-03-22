bb41e4563741e5926e4ed71ecdbd8d30f9d95e61
Mike <phegan@gmail.com>
2016-03-22 18:56:52 -0400

    Changed the class name for Redis

--- models/game.py
+++ models/game.py
@@ -1,9 +1,9 @@
-from tabletop_combat_manager.models.redis import Redis
+from tabletop_combat_manager.models.redis import Service
 
 
 class Model(object):
     def __init__(self):
-        self.redis = Redis()
+        self.redis = Service()
 
     def get(self, game_id):
         return self.redis.get(game_id)
